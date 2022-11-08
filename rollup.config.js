068385c279814e4855ffd8217d37aacdb59bd8c8
wangzhx <1132154046@qq.com>
2022-11-08 11:02:19 +0800

    fix: eslint indent

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+const typescript = require('@rollup/plugin-typescript')
+
+module.exports = {
+  input: 'src/main.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs'
+  },
+  plugins: [
+    typescript({
+      exclude: 'node_modules/**',
+      compilerOptions: {
+        lib: ['es5', 'es6', 'dom', 'esnext'],
+        target: 'esnext'
+      }
+    })
+  ]
+}
